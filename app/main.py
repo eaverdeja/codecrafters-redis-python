945884a5bea29f125f2c51b1524d2c4d4633e6e0
eaverdeja <eaverdeja@gmail.com>
2025-01-15 14:43:35 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,9 +1,28 @@
 import socket
 
+BUFFER_SIZE_BYTES = 1024
+CRLF = "\r\n"
+
 
 def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()
+    server_socket = socket.create_server(("localhost", 6379))
+
+    print("Listening on port 6379")
+    try:
+        while True:
+            connection, _addr = server_socket.accept()
+
+            request = connection.recv(BUFFER_SIZE_BYTES).decode()
+            lines = request.split(CRLF)
+            if "PING" in lines:
+                response = f"+PONG{CRLF}".encode()
+                connection.send(response)
+
+            connection.close()
+    except KeyboardInterrupt:
+        print("Shutting down server")
+    finally:
+        server_socket.close()
 
 
 if __name__ == "__main__":
